83e7bf429c7a6ae67b0b942548036d9368cc60e7
xensey <sey2@ya.ru>
2016-12-22 15:42:00 +0300

    table

--- php/users_answers/table.php
+++ php/users_answers/table.php
@@ -22,9 +22,7 @@ while ( $row = mysqli_fetch_array($query) ) {
     array_push($uids, $row[0]);
 	
 }
-echo "<pre>";
-print_r($uids);
-echo "</pre>";
+
 
 foreach ($uids as $id) {
     $query = mysqli_query($link, "SELECT question_id, value, answer_id, text_value, visit
@@ -33,6 +31,7 @@ foreach ($uids as $id) {
     while ( $row = mysqli_fetch_assoc($query) ) {
         if ($row['text_value']) {
             $value = $row['text_value'];
+			
         } else {
             $value = $row['value'];
         }
@@ -51,5 +50,95 @@ foreach ($uids as $id) {
     }
 }
 
-print_r($result);
+
+for($i = 1; $i <= 3; $i++) {
+    $query = mysqli_query($link, "SELECT id, name, s, q, v FROM questions");
+
+    while ( $row = mysqli_fetch_assoc($query) ) {
+
+        if ($row['q'] / 10 < 1) {
+            $q = '0' . $row['q'];
+        } else {
+            $q = $row['q'];
+        }
+        
+        if ($i == 1) {
+            $v = $row['v'] ? 'v' . $i : 'b';
+            $result[$i . $row['s'] . $q . $row['name']] = [
+                "var"   => $row['name'] . '_s' . $row['s'] . 'q' . $row['q'] . $v,
+                "id"    => $row['id'],
+                "visit" => $i
+            ];
+			 $question_vars[$i . $row['s'] . $q . $row['name']] = [
+                "var"   => $row['name'] . '_s' . $row['s'] . 'q' . $row['q'] . $v,
+                "id"    => $row['id'],
+                "visit" => $i
+            ];
+        } else {
+            if ($row['v']) {
+                $v = 'v' . $i;
+                $result[$i . $row['s'] . $q . $row['name']] = [
+                    "var"   => $row['name'] . '_s' . $row['s'] . 'q' . $row['q'] . $v,
+                    "id"    => $row['id'],
+                    "visit" => $i
+                ];
+				$question_vars[$i . $row['s'] . $q . $row['name']] = [
+                    "var"   => $row['name'] . '_s' . $row['s'] . 'q' . $row['q'] . $v,
+                    "id"    => $row['id'],
+                    "visit" => $i
+                ];
+            }
+        }
+    }
+	
+}
+echo "<pre>";
+foreach ($question_vars as $var ) {
+ print_r ($var['var']);echo "<br>";
+ }
+echo "</pre>";
+
+
+for($i = 1; $i <= 3; $i++) {
+
+    $query = mysqli_query($link, "SELECT id, variable FROM answers");
+
+    $test = array();
+
+    while ( $row = mysqli_fetch_assoc($query) ) {
+
+        if ($row['variable']) {
+            preg_match('/([A-Za-z0-9]+)_\w(\d{1,2})\w(\d{1,2})(\w{1,2})/', $row['variable'], $matches);
+
+            $v = $matches[4] ? $matches[4] : 'b';
+
+            if ($matches[3] / 10 < 1) {
+                $q = '0' . $matches[3];
+            } else {
+                $q = $matches[3];
+            }
+
+            if ($i == 1) {
+                $v = $matches[4] ? 'v' . $i : 'b';
+                $result[$i . $matches[2] . $q . $matches[1]] = [
+                    "var"   => $matches[1] . '_s' . $matches[2] . 'q' . $matches[3] . $v, 
+                    "id"    => $row['id'],
+                    "visit" => $i,
+                    "is_multiple" => true
+                ];
+            } else {
+                if ($matches[4]) {
+                    $v = 'v' . $i;
+                    $result[$i . $matches[2] . $q . $matches[1]] = [
+                        "var"   => $matches[1] . '_s' . $matches[2] . 'q' . $matches[3] . $v, 
+                        "id"    => $row['id'],
+                        "visit" => $i,
+                        "is_multiple" => true
+                    ];
+                }
+            }
+        }
+    }
+}
+
 ?>
